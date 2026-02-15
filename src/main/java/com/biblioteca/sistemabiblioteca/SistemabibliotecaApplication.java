d6bfdbfb1f45078e34251a00ac73f64726bec83b
Javier Pedraza <javierpedrazasan1@gmail.com>
2026-02-15 17:13:33 -0600

    Primer commit: Sistema funcionando con conexión a MySQL

---
+++ src/main/java/com/biblioteca/sistemabiblioteca/SistemabibliotecaApplication.java
@@ -0,0 +1,12 @@
+package com.biblioteca.sistemabiblioteca;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemabibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemabibliotecaApplication.class, args);
+	}
+
+}
