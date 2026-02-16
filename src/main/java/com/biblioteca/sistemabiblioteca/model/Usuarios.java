f72e41cdc5c7b377bf89cf8941172bc13307ca0c
Javier Pedraza <javierpedrazasan1@gmail.com>
2026-02-16 15:52:14 -0600

    Tablas actualizadas usuarios y libros

---
+++ src/main/java/com/biblioteca/sistemabiblioteca/model/Usuarios.java
@@ -0,0 +1,76 @@
+package com.biblioteca.sistemabiblioteca.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombreCompleto;
+
+    @Column(unique = true)
+    private String correoElectronico;
+
+    private String tipoUsuario; // Estudiante, Profesor, Administrativo
+
+    private LocalDate fechaRegistro;
+
+    private String estado; // Activo/Inactivo
+
+    // SETTERS AND GETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    
+}
